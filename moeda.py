22cd75c225a34a18b505d5b7b8f7a31182cc5c97
RaimundoHarla <raimundoharla899@gmail.com>
2025-05-08 13:57:19 -0300

    adicionando a função formatar internamente nas outras funções e docstrings

--- moeda.py
+++ moeda.py
@@ -1,22 +1,27 @@
-def aumentar(preço=0, taxa=0):
+def aumentar(preço=0, taxa=0, formato=False):
+    """Retorna o preço aumentado pela taxa percentual."""
     res = preço + (preço * taxa/100)
-    return res
+    return res if not formato else formatar(res)
 
 
-def diminuir(preço=0, taxa=0):
+def diminuir(preço=0, taxa=0, formato=False):
+    """Retorna o preço diminuído pela taxa percentual."""
     res = preço - (preço * taxa/100)
-    return res
+    return res if not formato else formatar(res)
 
 
-def dobro(preço=0):
+def dobro(preço=0, formato=False):
+    """Retorna o dobro do preço."""
     res = preço * 2
-    return res
+    return res if not formato else formatar(res)
 
 
-def metade(preço=0):
+def metade(preço=0, formato=False):
+    """Retorna a metade do preço."""
     res = preço / 2
-    return res
+    return res if not formato else formatar(res)
 
 
-def formatmoeda(preço=0, moeda='R$'):
+def formatar(preço=0, moeda='R$'):
+    """Formata o valor como o real."""
     return f'{moeda}{preço:.2f}'.replace('.', ',')
--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {moeda.formatmoeda(p)} é {moeda.formatmoeda(moeda.metade(p))}')
-print(f'O dobro de {moeda.formatmoeda(p)} é {moeda.formatmoeda(moeda.dobro(p))}')
-print(f'Aumentando 10%, temos {moeda.formatmoeda(moeda.aumentar(p, 10))}')
-print(f'Diminuindo 20%, temos {moeda.formatmoeda(moeda.diminuir(p, 20))}')
+print(f'A metade de {p} é {moeda.metade(p, True)}')
+print(f'O dobro de {p} é {moeda.dobro(p, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p, 10, True)}')
+print(f'Diminuindo 20%, temos {moeda.diminuir(p, 20, True)}')
