e3c66b84638f3293de9ccf6ac6de7f2da7569c34
RaimundoHarla <raimundoharla899@gmail.com>
2025-05-07 21:03:47 -0300

    correção na formatação do valor monetario

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de R${p} é R${moeda.metade(p)}')
-print(f'O dobro de R${p} é R${moeda.dobro(p)}')
-print(f'Aumentando 10%, temos R${p} é R${moeda.aumentar(p, 10)}')
-print(f'Diminuindo 20%, temos R${p} é R${moeda.diminuir(p, 20)}')
+print(f'A metade de {moeda.formatmoeda(p)} é {moeda.formatmoeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.formatmoeda(p)} é {moeda.formatmoeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos {moeda.formatmoeda(moeda.aumentar(p, 10))}')
+print(f'Diminuindo 20%, temos {moeda.formatmoeda(moeda.diminuir(p, 20))}')
