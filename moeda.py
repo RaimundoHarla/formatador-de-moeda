8c81ffe7c29c3fd931416eecaee3d998d04de997
RaimundoHarla <raimundoharla899@gmail.com>
2025-05-09 13:06:13 -0300

    reduz o codigo principal criando uma função que utiliza todas as funções anteriores

--- moeda.py
+++ moeda.py
@@ -25,3 +25,19 @@ def metade(preço=0, formato=False):
 def formatar(preço=0, moeda='R$'):
     """Formata o valor como o real."""
     return f'{moeda}{preço:.2f}'.replace('.', ',')
+
+
+def resumo(preço=0, taxaau=10, taxare=5):
+    """
+    Exibe um resumo formatado com análises do preço:
+    dobro, metade, aumento e redução percentual.
+    """
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: {formatar(preço)}')
+    print(f'Dobro do preço: {dobro(preço, True)}')
+    print(f'Metade do preço: {metade(preço, True)}')
+    print(f'{taxaau}% de aumento: {aumentar(preço, taxaau, True)}')
+    print(f'{taxare}% de redução: {diminuir(preço, taxare, True)}')
+    print('-' * 30)
